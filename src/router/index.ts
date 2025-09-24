2602f6f5a1feb9d7bfc3aec9700f72a7debf0760
TieR89 <tier8@yandex.ru>
2025-09-25 02:53:10 +0300

    fix(router): use absolute alias paths for dynamic imports to resolve fetch error

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: () => import('../views/HomeView.vue'),
+      component: () => import('@/views/HomeView.vue'),
     },
     {
       path: '/form-a',
       name: 'formA',
-      component: () => import('../views/FormAView.vue'),
+      component: () => import('@/views/FormAView.vue'),
     },
     {
       path: '/form-b',
       name: 'formB',
-      component: () => import('../views/FormBView.vue'),
+      component: () => import('@/views/FormBView.vue'),
     },
     {
       path: '/result',
       name: 'result',
-      component: () => import('../views/ResultView.vue'),
+      component: () => import('@/views/ResultView.vue'),
     },
   ],
 })
